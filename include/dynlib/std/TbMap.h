3990c16449a34b89367556b1517e2066b4258600
BREAK350 <break350@gmail.com>
2015-02-27 14:50:29 +0200

    add empty class TbMap

---
+++ include/dynlib/std/TbMap.h
@@ -0,0 +1,37 @@
+//---------------------------------------------------------------------------
+#ifndef TbMapH
+#define TbMapH
+//---------------------------------------------------------------------------
+// includes
+
+//---------------------------------------------------------------------------
+namespace dynlib
+{
+	namespace std
+	{
+		class TbMap
+		{
+			// fields
+			private:
+			protected:
+			public:
+
+			// static fields
+			private:
+			protected:
+			public:
+
+			// methods
+			private:
+			protected:
+			public:
+
+			// static methods
+			private:
+			protected:
+			public:
+	   };
+	}
+}
+//---------------------------------------------------------------------------
+#endif
