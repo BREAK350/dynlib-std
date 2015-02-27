ef39e9c90ae9f616ed36dbf3bf3451f4ea871bbf
BREAK350 <break350@gmail.com>
2015-02-27 23:00:09 +0200

    make format for class TbHashMap, using ctrl+d

--- include/dynlib/collections/TbHashMap.cpp
+++ include/dynlib/collections/TbHashMap.cpp
@@ -1,132 +1,108 @@
-//---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 #pragma hdrstop
 #include "TbHashMap.h"
 #pragma package(smart_init)
-//---------------------------------------------------------------------------
-namespace dynlib
-{
-	namespace collections
-	{
-		TbHashMap::TbHashMap()
-		{
+
+// ---------------------------------------------------------------------------
+namespace dynlib {
+	namespace collections {
+		TbHashMap::TbHashMap() {
 			count = 0;
 			roots_ = 0;
 			maxElements = 16;
 		}
 
-		TbHashMap::~TbHashMap()
-		{
+		TbHashMap::~TbHashMap() {
 
 		}
 
-		int TbHashMap::add_(TbHashMapItem *item_)
-		{
+		int TbHashMap::add_(TbHashMapItem *item_) {
 			int i = getIndexFromHash(item_->getHash());
-			if(i == -1)
-			{
+			if (i == -1) {
 
 			}
-			else
-			{
+			else {
 				int res = roots_[i]->add_(item_, maxElements);
-				if(!res)
-				{
+				if (!res) {
 					/*
-						resize
-                        count = 2 * count;
-					*/
-                }
-            }
+					resize
+					count = 2 * count;
+					 */
+				}
+			}
 		}
 
-		int TbHashMap::getIndexFromHash(const int &hash)const
-		{
-         	return count > 0 ? hash % count : -1;
-        }
+		int TbHashMap::getIndexFromHash(const int &hash)const {
+			return count > 0 ? hash % count : -1;
+		}
 	}
 }
-//---------------------------------------------------------------------------
-namespace dynlib
-{
-	namespace collections
-	{
-		TbHashMap::TbHashMapItem::~TbHashMapItem()
-		{
-			if(next_)
-			{
+
+// ---------------------------------------------------------------------------
+namespace dynlib {
+	namespace collections {
+		TbHashMap::TbHashMapItem::~TbHashMapItem() {
+			if (next_) {
 				delete next_;
 				next_ = 0;
 			}
 		}
 
-		TbHashMap::TbHashMapItem::TbHashMapItem()
-		{
-        	next_ = 0;
+		TbHashMap::TbHashMapItem::TbHashMapItem() {
+			next_ = 0;
 		}
 
-		int TbHashMap::TbHashMapItem::getHash()const
-		{
+		int TbHashMap::TbHashMapItem::getHash()const {
 			TbHashable *data = getData();
 			return data ? data->getHash() : -1;
 		}
 
-		void TbHashMap::TbHashMapItem::setNext_(TbHashMapItem *next_)
-		{
-			if(this->next_)
-			{
+		void TbHashMap::TbHashMapItem::setNext_(TbHashMapItem *next_) {
+			if (this->next_) {
 				delete next_;
 			}
 			this->next_ = next_;
 		}
 
-		TbHashMap::TbHashMapItem* TbHashMap::TbHashMapItem::getNext()const
-		{
-        	return next_;
-        }
+		TbHashMap::TbHashMapItem* TbHashMap::TbHashMapItem::getNext()const {
+			return next_;
+		}
 	}
 }
-//---------------------------------------------------------------------------
-namespace dynlib
-{
-	namespace collections
-	{
-		TbHashMap::TbHashMapRoot::TbHashMapRoot()
-		{
+
+// ---------------------------------------------------------------------------
+namespace dynlib {
+	namespace collections {
+		TbHashMap::TbHashMapRoot::TbHashMapRoot() {
 			count = 0;
 			root_ = 0;
-        }
+		}
 
-		TbHashMap::TbHashMapRoot::~TbHashMapRoot()
-		{
-			if(root_)
-			{
+		TbHashMap::TbHashMapRoot::~TbHashMapRoot() {
+			if (root_) {
 				delete root_;
 				root_ = 0;
 				count = 0;
-            }
-        }
+			}
+		}
 
-		int TbHashMap::TbHashMapRoot::add_(TbHashMapItem *item_, const int &maxElements)
-		{
-			if(item_)
-			{
+		int TbHashMap::TbHashMapRoot::add_(TbHashMapItem *item_,
+			const int &maxElements) {
+			if (item_) {
 				item_->setNext_(root_);
 				root_ = item_;
 				++count;
 			}
 			return count < maxElements;
-        }
+		}
 
-		TbHashable* TbHashMap::TbHashMapRoot::get(const int &hash)const
-		{
+		TbHashable* TbHashMap::TbHashMapRoot::get(const int &hash)const {
 			TbHashMapItem *item = root_;
-			while(item && item->getHash() != hash)
-			{
-                item = item->getNext();
+			while (item && item->getHash() != hash) {
+				item = item->getNext();
 			}
 			return item ? item->getData() : 0;
-        }
+		}
 	}
 }
-//---------------------------------------------------------------------------
-
+// ---------------------------------------------------------------------------
--- include/dynlib/collections/TbHashMap.h
+++ include/dynlib/collections/TbHashMap.h
@@ -1,83 +1,80 @@
-//---------------------------------------------------------------------------
+// ---------------------------------------------------------------------------
 #ifndef TbHashMapH
 #define TbHashMapH
-//---------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------
 // classes
-namespace dynlib
-{
-	namespace collections
-	{
+namespace dynlib {
+	namespace collections {
 		class TbHashMap;
 		class TbHashable;
 	}
 }
-//---------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------
 // includes
-//---------------------------------------------------------------------------
-namespace dynlib
-{
-	namespace collections
-	{
-		class TbHashMap
-		{
+// ---------------------------------------------------------------------------
+namespace dynlib {
+	namespace collections {
+		class TbHashMap {
+		private:
+			// classes
+			class TbHashMapItem {
 			private:
-				// classes
-				class TbHashMapItem
-				{
-					private:
-						TbHashMapItem *next_;
-					public:
-                        TbHashMapItem();
-						virtual ~TbHashMapItem();
-						virtual TbHashable* getData()const = 0;
-						virtual int getHash()const;
+				TbHashMapItem *next_;
 
-						virtual void setNext_(TbHashMapItem *next_);
-						virtual TbHashMapItem* getNext()const;
-				};
+			public:
+				TbHashMapItem();
+				virtual ~TbHashMapItem();
+				virtual TbHashable* getData()const = 0;
+				virtual int getHash()const;
 
-				class TbHashMapRoot
-				{
-					private:
-						int count;
-						TbHashMapItem *root_;
-					public:
-						TbHashMapRoot();
-						virtual ~TbHashMapRoot();
+				virtual void setNext_(TbHashMapItem *next_);
+				virtual TbHashMapItem* getNext()const;
+			};
 
-						virtual int add_(TbHashMapItem *item_, const int &maxElements);
-						virtual TbHashable* get(const int &hash)const;
-				};
-
+			class TbHashMapRoot {
 			private:
 				int count;
-				TbHashMapRoot **roots_;
+				TbHashMapItem *root_;
 
-				int maxElements;
-			private:
-				virtual int add_(TbHashMapItem *item_);
-				virtual int getIndexFromHash(const int &hash)const;
 			public:
-				TbHashMap();
-				virtual ~TbHashMap();
+				TbHashMapRoot();
+				virtual ~TbHashMapRoot();
 
-				virtual int add(TbHashable *data);
-				virtual int add_(TbHashable *data_);
-        };
-	}
-}
-//---------------------------------------------------------------------------
-namespace dynlib
-{
-	namespace collections
-	{
-		class TbHashable
-		{
-			public:
-				virtual ~TbHashable() {};
-				virtual int getHash()const = 0;
+				virtual int add_(TbHashMapItem *item_, const int &maxElements);
+				virtual TbHashable* get(const int &hash)const;
+			};
+
+		private:
+			int count;
+			TbHashMapRoot **roots_;
+
+			int maxElements;
+
+			virtual int add_(TbHashMapItem *item_);
+			virtual int getIndexFromHash(const int &hash)const;
+
+		public:
+			TbHashMap();
+			virtual ~TbHashMap();
+
+			virtual int add(TbHashable *data);
+			virtual int add_(TbHashable *data_);
 		};
 	}
 }
-//---------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------
+namespace dynlib {
+	namespace collections {
+		class TbHashable {
+		public:
+			virtual ~TbHashable() {
+			};
+			virtual int getHash()const = 0;
+		};
+	}
+}
+// ---------------------------------------------------------------------------
 #endif
